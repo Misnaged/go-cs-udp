c194b4bc4a7ab05760d73d59e81e27cf20d95a2f
Misnaged <pa100ni@gmail.com>
2021-10-16 04:53:19 +0300

    new

---
+++ csharp-cli/UDP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UDPcli();
+        }
+
+        public static void UDPcli()
+        {
+UdpClient udpClient = new UdpClient();
+
+Byte[] sendBytes = Encoding.ASCII.GetBytes("HOWDY SEREGA!!!"); 
+try{
+    udpClient.Send(sendBytes, sendBytes.Length, "127.0.0.1", 8080);
+}
+catch ( Exception e ){
+    Console.WriteLine(e.ToString());	
+}
+        }
+    }
+}
+    
